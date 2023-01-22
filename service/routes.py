ff8fe53e6edf07893dca1e32688372a9e2f63af9
Joguima24 <118137336+Joguima24@users.noreply.github.com>
2023-01-22 12:00:37 +0000

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -63,6 +63,8 @@ def create_accounts():
 
 # ... place you code here to LIST accounts ...
 @app.route("/accounts", methods=["GET"])
+
+
 def list_accounts():
     app.logger.info("Request to list accounts")
 
@@ -77,8 +79,11 @@ def list_accounts():
 # READ AN ACCOUNT
 ######################################################################
 
+
 # ... place you code here to READ an account ...
 @app.route("/accounts/<int:account_id>", methods=["GET"])
+
+
 def get_accounts(account_id):
     app.logger.info("Request to read an Account with id: %s", account_id)
 
@@ -96,13 +101,11 @@ def get_accounts(account_id):
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     app.logger.info("Request to update an Account with id: %s", account_id)
-
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     account.deserialize(request.get_json())
     account.update()
-    
     return account.serialize(), status.HTTP_200_OK
 
 ######################################################################
@@ -111,13 +114,13 @@ def update_accounts(account_id):
 
 # ... place you code here to DELETE an account ...
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
+
+
 def delete_accounts(account_id):
     app.logger.info("Request to delete an Account with id: %s", account_id)
-
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-    
     account.delete()
     return "", status.HTTP_204_NO_CONTENT
 
